d766b638d995261c6aab4aaa35162cc57cf76f6e
rakita <rakita@users.noreply.github.com>
2021-03-19 16:53:56 +0100

    devp2p to reth repo and rakita branch

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-// Copyright 2020 Gnosis Ltd.
-// SPDX-License-Identifier: Apache-2.0
-
 use std::{sync::Arc, thread, time::Duration};
 
-use reth_scheduler::{
-    client_adapter::client_info::{Client, Snapshot},
-    devp2p_adapter::*,
-    Scheduler,
+use interfaces::{
+    blockchain::BlockchainReadOnly, devp2p::*, importer::Importer, snapshot::Snapshot,
 };
+use scheduler::Scheduler;
 
+mod dummy_impl;
+use dummy_impl::*;
 mod oe_adapter;
 use oe_adapter::*;
 
@@ -17,14 +15,9 @@ extern crate simple_logger;
 use log::*;
 use simple_logger::SimpleLogger;
 
-pub struct DummyClient();
-impl Client for DummyClient {}
-
-pub struct DummySnapshot();
-impl Snapshot for DummySnapshot {}
-
 fn main() {
-    SimpleLogger::new().with_level(LevelFilter::Info)
+    SimpleLogger::new()
+        .with_level(LevelFilter::Info)
         .with_module_level("mio", LevelFilter::Warn)
         .with_module_level("discovery", LevelFilter::Info)
         .with_module_level("network", LevelFilter::Info)
@@ -32,11 +25,17 @@ fn main() {
         .unwrap();
 
     info!("Starting organizer");
-    let client = Arc::new(DummyClient {});
+    let blockchain = Arc::new(BlockchainInMemory::new());
+    let importer = Arc::new(DummyImporter {});
     let snapshot = Arc::new(DummySnapshot {});
     let devp2p = OeDevp2p::new().unwrap();
     //devp2p.
-    let org = Scheduler::new(Box::new(devp2p), client.clone(), snapshot.clone());
+    let org = Scheduler::new(
+        Box::new(devp2p),
+        blockchain.clone(),
+        importer.clone(),
+        snapshot.clone(),
+    );
     org.start();
 
     thread::sleep(Duration::from_secs(300));
